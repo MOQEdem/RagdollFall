719ff0ecd4f7d0f2cfad36c35f921efe964cddc4
MOQEdem <smirnov_vladimir_svv@inbox.ru>
2023-08-14 18:27:44 +0500

    ForstBugPackFixed

--- Assets/Scripts/ActivatorOfFinish.cs
+++ Assets/Scripts/ActivatorOfFinish.cs
@@ -7,5 +7,7 @@ public class ActivatorOfFinish : MonoBehaviour
     private void OnTriggerEnter(Collider other)
     {
         SceneManager.LoadScene(0);
+        Time.timeScale = 1;
+        Time.fixedDeltaTime = 0.02f;
     }
 }
--- Assets/Scripts/SlowMoController.cs
+++ Assets/Scripts/SlowMoController.cs
@@ -6,6 +6,7 @@ public class SlowMoController : MonoBehaviour
     [SerializeField] private CameraController _cameraController;
 
     private Coroutine _slowingTime;
+    private float _normalFixedDeltaTimeScale = 0.02f;
 
     private void OnEnable()
     {
@@ -33,8 +34,13 @@ public class SlowMoController : MonoBehaviour
         {
             timeToSlowDone -= Time.deltaTime;
 
+            Debug.Log(Time.fixedDeltaTime);
+
             if (Time.timeScale >= 0.5)
                 Time.timeScale -= Time.deltaTime;
+
+            if (Time.fixedDeltaTime >= 0.01f)
+                Time.fixedDeltaTime -= Time.deltaTime * _normalFixedDeltaTimeScale;
 
             yield return null;
         }
@@ -50,10 +56,14 @@ public class SlowMoController : MonoBehaviour
             if (Time.timeScale < 1)
                 Time.timeScale += Time.deltaTime;
 
+            if (Time.fixedDeltaTime < 0.02f)
+                Time.fixedDeltaTime += Time.deltaTime * _normalFixedDeltaTimeScale;
+
             yield return null;
         }
 
         Time.timeScale = 1;
+        Time.fixedDeltaTime = 0.02f;
         _slowingTime = null;
     }
 }
